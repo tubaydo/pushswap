941f0a5f1581d47000e99411e617c8e25681c51a
Tuba Aydoğan <111774892+tubaydo@users.noreply.github.com>
2023-02-24 20:32:21 +0300

    Add files via upload

---
+++ push_swap/Libft/ft_memcpy.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tubaaydogan <tubaaydogan@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/31 18:14:53 by okirca            #+#    #+#             */
+/*   Updated: 2023/02/21 17:53:01 by tubaaydogan      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/libft.h"
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t	i;
+
+	if (!dst && !src)
+	{
+		return (0);
+	}
+	i = 0;
+	while (i < n)
+	{
+		((unsigned char *) dst)[i] = ((unsigned char *) src)[i];
+		i++;
+	}
+	return (dst);
+}
