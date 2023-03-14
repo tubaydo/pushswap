d48b1735757d5f7ac24100c8503d37b57e51475d
Tuba Aydoğan <111774892+tubaydo@users.noreply.github.com>
2023-03-14 19:11:18 +0300

    Add files via upload

---
+++ push_swap/inc/push_swap.h
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: taydogan <taydogan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/21 17:45:39 by tubaaydogan       #+#    #+#             */
+/*   Updated: 2023/03/14 15:55:50 by taydogan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+# ifndef PUSH_SWAP_H
+#define PUSH_SWAP_H
+
+
+#define NULL __DARWIN_NULL
+
+#define fence __atomic_thread_fence
+#define RELEASE __ATOMIC_RELEASE
+#define ACQUIRE __ATOMIC_ACQUIRE
+
+#include "libft.h"
+#include "stdio.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct stack{
+    int *a;
+    int size_a;
+    int *b;
+    int size_b;
+}stack;
+
+int     *makeArr(int size,char **arr);
+void    pushSwap(int argc, int *arr);
+void    intExceed(int argc,char **argv);
+void    isTheSame(int argc,char **argv);
+void    isnt_int(int argc,char **argv);
+void    pa(stack *s);
+void    pb(stack *s);
+void    pop_a(stack *s);
+void    pop_b(stack *s);
+void    sa(stack *s);
+void    sb(stack *s);
+void    ss(stack *s);
+void    reverse(int* arr, int i, int j);
+void    rotate(int* nums, int numsSize, int k);
+void    ra(stack *s);
+void    rb(stack *s);
+void    rr(stack *s);
+void    rra(stack *s);
+void    rrb(stack *s);
+void    rrr(stack *s);
+int     error();
+void    quicksort(stack *s,int size,int *arr);
+void    basecase_2b(stack *s);
+void    basecase_2a(stack *s);
+void    sa_ctrl(stack *s);
+void    uptoindex(stack *s,int pivotindex,int size);
+int     unsorted(int *s,int c);
+
+#endif
---
+++ push_swap/src/push.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: taydogan <taydogan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/25 00:49:09 by tubaaydogan       #+#    #+#             */
+/*   Updated: 2023/03/14 16:14:15 by taydogan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/push_swap.h"
+
+void pop_a(stack *s)
+{
+	if(s->size_a == -1)
+		exit(1);
+	else
+	{
+		s->a[s->size_a] = NULL;
+		s->size_a--;
+	}
+	
+}
+void pop_b(stack *s)
+{
+	if(s->size_b == -1)
+		exit(1);
+	else
+	{
+		s->b[s->size_b] = NULL;
+		s->size_b--;
+	}
+	
+}
+void pa(stack *s)
+{
+	s->size_a++;
+	s->a[s->size_a] = s->b[s->size_b];
+	pop_b(s);
+	write(1,"pa\n",3);
+}
+
+void pb(stack *s)
+{
+	s->size_b++;
+	s->b[s->size_b] = s->a[s->size_a];
+	pop_a(s);
+	write(1,"pb\n",3);
+}
+
+// void sa()
+// {
+// printf("a nın top %d\n",s->size_a);
+// 	printf("b nın top %d\n",s->size_b);
+// 	s->b[s->size_b] = s->a[s->size_a];
+// 	s->size_a--;
+// 	s->size_b++;
+// 	printf("b nın top elemanı %d\n",s->b[-1]);
+// 	write(1,"pb\n",3);
+	
+// } 
+// void pb(stack *s,int c,int argc)
+// {
+// 	if(s->size_b == argc -1)
+// 		exit(1);
+// 	else
+// 	{
+// 		s->size_b++;
+// 		s->b[s->size_b] = c;
+// 	}
+// 	pop_a(s);
+// 	write(1,"pb\n",3);
+// }
